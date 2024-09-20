6cbffbefee014d25e4f9e9f3f832995a1a0c1d18
Arthur Junio <130364490+ArthurJ26@users.noreply.github.com>
2024-09-20 12:09:01 -0300

    Delete Site directory

--- Site/js/header.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function(){
-    let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 200)
-})
-
-function actionToggle(){
-    var action = document.querySelector('.action');
-    action.classList.toggle('active')
-}
